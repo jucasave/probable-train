6ffc6c47a08ede2a7e875dbeb601627aa20648dd
Carlos <160746980+jucasave@users.noreply.github.com>
2024-06-15 23:40:34 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,109 @@
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 8;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.box-container .box')];
+    for(var i = currentItem; i < currentItem + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem += 4;
+    if(currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none'
+    }
+}
+
+//carrito
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+function comprarElemento(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+
+function leerDatosElemento(elemento) {
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoElemento);
+}
+function insertarCarrito(elemento) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>
+       <img src="${elemento.imagen}" width=100 height=150px >
+    </td>
+    <td>
+        ${elemento.titulo}
+    </td>
+    <td>
+        ${elemento.precio}
+    </td>
+    <td>
+        <a herf="#" class="borrar" data-id="${elemento.id}" >x</a>
+    </td>
+    `;
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e) {
+    e.preventDefault();
+    let elemento,
+        elementoId;
+    
+    if(e.target.classList.contains('borrar')) {
+       e.target.parentElement.parentElement.remove();
+       elemento = e.target.parentElement.parentElement;
+       elementoId = elemento.querySelector('a').getAttribute('data-id');
+    }
+}
+
+function vaciarCarrito() {
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
+}
+
+// swith
+const swith = document.querySelector('.switch');
+
+swith.addEventListener('click', e => {
+    swith.classList.toggle('active');
+    document.body.classList.toggle("active");
+})
+
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    opciones[5].className = "";
+    opciones[6].className = "";
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
