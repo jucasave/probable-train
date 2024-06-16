f0199ca6e67b235b27380ff63445efc8936361a6
Carlos <160746980+jucasave@users.noreply.github.com>
2024-06-15 23:46:57 -0600

    Add files via upload

---
+++ MedioAmbiente/script.js
@@ -0,0 +1,7 @@
+// swith
+const swith = document.querySelector('.switch');
+
+swith.addEventListener('click', e => {
+    swith.classList.toggle('active');
+    document.body.classList.toggle("active");
+})
